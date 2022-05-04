bd7d1425ffb518ac244aeb88b0b891fc7240553e
Alexsandron3 <alexsandro0602099@outlook.com>
2022-05-04 12:07:01 -0300

    Cria controllers para empresas e usuarios

---
+++ app/Http/Controllers/UsuariosController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuarios;
+use Illuminate\Http\Request;
+
+class UsuariosController extends Controller
+{
+    public function listarTodos()
+    {
+        $usuario = Usuarios::join('empresas_usuarios', 'empresas_usuarios.idUsuario', '=', 'usuarios.id')
+            ->join('empresas', 'empresas.id', '=', 'empresas_usuarios.idEmpresa')
+            ->select(
+                Usuarios::raw('usuarios.id, usuarios.nome, usuarios.email, usuarios.telefone, usuarios.cidadeNascimento, usuarios.dataNascimento'),
+                Usuarios::raw('GROUP_CONCAT(empresas.nome) AS nomeEmpresa')
+            )
+            ->groupBy('usuarios.id')
+            ->get();
+        return $usuario;
+    }
+}

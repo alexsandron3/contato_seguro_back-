1558b901b2133f8a69415e649b8f8d5f890db0b5
Alexsandron3 <alexsandro0602099@outlook.com>
2022-05-04 12:06:46 -0300

    Cria grupo de rotas para usuarios

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+// Rota para usuários
+
+
+/**
+ * Lista todos usuários com as empresas
+ */
+
+$router->group(['prefix' => 'usuarios'], function () use ($router) {
+    $router->get('/', 'UsuariosController@listarTodos');
+
+});
